5e71293ad0afce6625dd73d1b6df88f218c7942d
TIITIIGroz <team.grozopic@gmail.com>
2026-08-03 13:21:59 +0200

    Create links.js

---
+++ commands/links.js
@@ -0,0 +1,50 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require("discord.js");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName("links")
+        .setDescription("Affiche tous les liens de TIITII_Groz"),
+
+    async execute(interaction) {
+
+        const embed = new EmbedBuilder()
+
+            .setColor(0x9146FF)
+
+            .setTitle("🔗 Les liens de TIITII_Groz")
+
+            .setDescription(
+`*Salut ! Voici comme demandé les liens de* **𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛**
+
+*Hey! Here are the socials for* **𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛**
+
+🌐 **Tous ses réseaux / All his links**
+https://beacons.ai/TIITII_Groz
+
+💜 **Twitch**
+https://twitch.tv/TIITII_Groz
+
+❤️ **YouTube**
+https://youtube.com/@TIITII_Groz
+
+🎵 **TikTok**
+https://tiktok.com/@TIITII_Groz`
+)
+
+            .setFooter({
+                text: "TIITII_Groz"
+            })
+
+            .setTimestamp();
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+
+    }
+
+};
