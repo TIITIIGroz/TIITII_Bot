eb57955e82aae3d198ec1d5a1e8404b1caf93b71
TIITIIGroz <team.grozopic@gmail.com>
2026-08-03 18:26:07 +0200

    Update links.js

--- commands/links.js
+++ commands/links.js
@@ -15,28 +15,28 @@ module.exports = {
 
             .setColor(0x9146FF)
 
-            .setTitle("🔗 Les liens de 𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛 - Social media of 𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛")
+            .setTitle("🔗 Les liens de 𝑻𝑰𝑰𝑻𝑰𝑰𝑮𝒓𝒐𝒛 - Social media of 𝑻𝑰𝑰𝑻𝑰𝑰𝑮𝒓𝒐𝒛")
 
             .setDescription(
-`**Salut ! Voici comme demandé les liens de** 𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛
+`**Salut ! Voici comme demandé les liens de** 𝑻𝑰𝑰𝑻𝑰𝑰𝑮𝒓𝒐𝒛
 
-**Hey! Here are the socials for** 𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛
+**Hey! Here are the socials for** 𝑻𝑰𝑰𝑻𝑰𝑰𝑮𝒓𝒐𝒛
 
 🌐 **Tous ses réseaux / All his links:**
-https://beacons.ai/𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛
+https://beacons.ai/TIITII_Groz
 
 🎥 **Twitch:**
-https://twitch.tv/𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛
+https://twitch.tv/TIITII_Groz
 
 🎬 **YouTube:**
-https://youtube.com/@𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛
+https://youtube.com/@TIITII_Groz
 
 📸 **TikTok:**
-https://tiktok.com/@𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛`
+https://tiktok.com/@TIITII_Groz`
 )
 
             .setFooter({
-                text: "𝑻𝑰𝑰𝑻𝑰𝑰_𝑮𝒓𝒐𝒛"
+                text: "TIITII_Groz"
             })
 
             .setTimestamp();
