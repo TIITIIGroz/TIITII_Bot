8e414002df5129f1343fdd5b023c62e3d884c47e
TIITIIGroz <team.grozopic@gmail.com>
2026-08-20 17:53:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on(Events.MessageCreate, async (message) => {
 });
 
 // Bot prêt
-client.once(Events.Clientready, () => {
+client.once(Events.ClientReady, () => {
     console.log(`✅ Connecté en tant que ${client.user.tag}`);
     
     // 👇 VÉRIFICATION DES COMMANDES EN MÉMOIRE 👇
