ce14ecbe76195219c04c1ab9819153b1d82f8c87
TIITIIGroz <team.grozopic@gmail.com>
2026-08-20 06:11:15 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -27,25 +27,23 @@ for (const file of commandFiles) {
 
 console.log(`📋 ${commands.length} commande(s) trouvée(s).`);
 
-const rest = new REST({ version: "10" })
-    .setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        console.log("📈 Déploiement des commandes...");
+        console.log("🚀 Déploiement des commandes...");
+        console.log(`📤 Envoi de ${commands.length} commandes à Discord...`);
 
         const data = await rest.put(
             Routes.applicationCommands("1343093684832436367"),
-            {
-                body: commands
-            }
+            { body: commands }
         );
 
-        console.log(`✅ ${data.length} commande(s) déployée(s).`);
+        console.log(`✅ Succès ! ${data.length} commande(s) déployée(s).`);
         process.exit(0);
 
     } catch (error) {
-        console.error(":x: Erreur lors du déploiement :", error);
+        console.error("❌ ERREUR DISCORD DÉTAILLÉE :", error);
         process.exit(1);
     }
 })();
