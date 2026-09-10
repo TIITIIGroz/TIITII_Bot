31c88ed0424a011678e2db91457bd5f3a8243a14
TIITIIGroz <team.grozopic@gmail.com>
2026-09-10 18:15:45 +0200

    Update ConfigReminder.js kk

--- Automatisme/ConfigReminder.js
+++ Automatisme/ConfigReminder.js
@@ -9,7 +9,7 @@ module.exports = (client) => {
         try {
             const targetChannel = await client.channels.fetch(targetChannelId);
             if (targetChannel) {
-                await targetChannel.send(`**Nouveau salon détecté !** Pense à l'ajouter dans \`config.js\` :\nNom : \`${channel.name}\`\nID : \`${channel.id}\``);
+                await targetChannel.send(`⚠️ **Nouveau salon détecté !** Pense à l'ajouter dans \`config.js\` :\n> **Nom :** \`${channel.name}\`\n> **ID :** \`${channel.id}\``);
             }
         } catch (error) {
             console.error("Erreur lors de l'envoi de l'alerte salon :", error);
@@ -21,7 +21,7 @@ module.exports = (client) => {
         try {
             const targetChannel = await client.channels.fetch(targetChannelId);
             if (targetChannel) {
-                await targetChannel.send(`**Nouveau rôle détecté !** Pense à l'ajouter dans \`config.js\` :\nNom : \`${role.name}\`\nID : \`${role.id}\``);
+                await targetChannel.send(`⚠️ **Nouveau rôle détecté !** Pense à l'ajouter dans \`config.js\` :\n> **Nom :** \`${role.name}\`\n> **ID :** \`${role.id}\``);
             }
         } catch (error) {
             console.error("Erreur lors de l'envoi de l'alerte rôle :", error);
