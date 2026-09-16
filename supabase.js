3228162a64ada29bad24eadb0e6e8164e385ab8c
TIITIIGroz <team.grozopic@gmail.com>
2026-09-16 21:25:08 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,14 @@
 const { createClient } = require('@supabase/supabase-js');
+const WebSocket = require('ws');
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY; // ou SUPABASE_ANON_KEY selon tes variables sur Render
+const supabaseKey = process.env.SUPABASE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: { persistSession: false },
+    realtime: {
+        transport: WebSocket,
+    },
+});
 
 module.exports = supabase;
