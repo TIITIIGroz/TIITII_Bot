afa068ff09f7d0f4dd167874aa21423f0da71e7d
TIITIIGroz <team.grozopic@gmail.com>
2026-09-23 13:13:06 +0200

    Update bday-rm.js

--- commands/bday-rm.js
+++ commands/bday-rm.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const supabase = '../supabase';
+const supabase = require('../supabase');
 
 module.exports = {
     data: new SlashCommandBuilder()
