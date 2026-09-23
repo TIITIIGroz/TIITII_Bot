0bb92d8fc7ad6fa25700da60147525982d6be60b
TIITIIGroz <team.grozopic@gmail.com>
2026-09-23 12:55:15 +0200

    Create bday-rm.js

---
+++ commands/bday-rm.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const supabase = require('../../supabase');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bday-rm')
+        .setDescription('Remove your registered birthday date'),
+    async execute(interaction) {
+        const ENGLISH_ROLE_ID = "1094758180938067989";
+        if (!interaction.member.roles.cache.has(ENGLISH_ROLE_ID)) {
+            return interaction.reply({
+                content: "❌ This command is restricted to members with the English language role.",
+                flags: [MessageFlags.Ephemeral]
+            });
+        }
+
+        const userId = interaction.user.id;
+
+        try {
+            const { error } = await supabase
+                .from('birthdays')
+                .delete()
+                .eq('user_id', userId);
+
+            if (error) throw error;
+
+            await interaction.reply({
+                content: "✅ Your birthday has been successfully removed from the database.",
+                flags: [MessageFlags.Ephemeral]
+            });
+        } catch (err) {
+            console.error("Erreur bday-rm :", err);
+            await interaction.reply({
+                content: "❌ An error occurred while deleting.",
+                flags: [MessageFlags.Ephemeral]
+            });
+        }
+    }
+};
