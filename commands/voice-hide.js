c7ce20858874218f25de3b41501bba9a70a6e59f
TIITIIGroz <team.grozopic@gmail.com>
2026-09-25 18:14:43 +0200

    Update voice-hide.js

--- commands/voice-hide.js
+++ commands/voice-hide.js
@@ -5,8 +5,8 @@ module.exports = {
         .setName('voice-hide')
         .setDescription('Cache ton salon vocal aux autres membres')
         .setDescriptionLocalizations({
-            fr: "Cache ton salon vocal personnalisé à tout le monde (sauf les admins). Tu dois être dans ton salon vocal ou dans le salon <#1533281900318167060>",
-            en: "Hides your custom voice channel from everyone (except admins). You must be in your voice channel or in <#1533281900318167060>"
+            fr: "Cache ton salon vocal aux autres membres (sauf les admins). Tu dois être dans ton salon vocal ou dans le salon <#1533281900318167060>",
+            en: "Hides your voice channel from members (except admins). You must be in your voice channel or in <#1533281900318167060>"
         }),
 
     async execute(interaction) {
